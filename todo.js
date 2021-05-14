4e0049a1167c61489ba7d3968fcbe74e964a4920
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-14 15:20:20 -0400

    function to create html template

--- todo.js
+++ todo.js
@@ -2,13 +2,28 @@ const add = document.querySelector('.add');
 const todo = document.querySelector('.todo');
 
 
+const templateForNewSprint=sprint=>{
+    const list=
+     `<li class='list-group-item d-flex justify-content-between align-items-center'>
+        <span>${sprint}</span>
+         <i class="fas fa-trash delete"></i>
+    </li>`  
+
+      todo.innerHTML +=list;
+}
+
+
 add.addEventListener('submit', event => {
 
     event.preventDefault();
     const sprint = add.addSprint.value;
     //console.log(sprint)
-    todo.innerHTML += ` <li class='list-group-item d-flex justify-content-between align-items-center'>
-                        <span>${sprint}</span>
-                        <i class="fas fa-trash delete"></i>
-                        </li>`
+    if (sprint.length) {
+      
+        templateForNewSprint(sprint)
+        add.reset();
+    }
+  
 })
+
+
